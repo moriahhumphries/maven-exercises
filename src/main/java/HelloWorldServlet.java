cc73a7108f5cc113d47ef37c6b658f71c7198122
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-02 16:20:25 -0500

    completed helloworld servlet practice

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet (name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        if (name == null) {
+            out.println("<h1>Hello World</h1>");
+        } else {
+            out.println("<h1>Hello, " + name + "</h1>");
+        }
+    }
+}
